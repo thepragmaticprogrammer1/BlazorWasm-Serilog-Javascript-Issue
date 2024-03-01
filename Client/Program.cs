36f64f30f5a53860ff33d54492592b7cd9f57cda
Tony Mould <smartmelonsystems@gmail.com>
2024-03-01 09:38:24 +0000

    first commit

---
+++ Client/Program.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BlazorAppNet8.Client;
+using Microsoft.JSInterop;
+using Serilog;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+string InstanceId = Guid.NewGuid().ToString("n");
+var logConfig = new LoggerConfiguration()
+    // .Enrich.WithProperty("InstanceId", InstanceId)
+    .WriteTo.BrowserConsole(outputTemplate: "%cserilog{_}color:white;background:#8c7574;border-radius:3px;padding:1px 2px;font-weight:600;{Level}[{SourceContext}]{Message}{NewLine}{Exception}");
+Log.Logger = logConfig.CreateLogger();
+
+// Remove this line and Javascript import works first time as expected
+Log.Information("App Instance Id:{InstanceId}", InstanceId);
+
+// builder.Services.AddLogging(loggingBuilder => loggingBuilder.AddSerilog(dispose: true));
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+var host = builder.Build();
+
+var js = host.Services.GetRequiredService<IJSRuntime>();
+
+// This should work....
+// var module = await js.InvokeAsync<IJSObjectReference>("import", "./localeHelper.js");
+// var message = await module.InvokeAsync<string>("showPrompt", "hello");
+
+
+// Workaround: Get the module in a loop - second try usually succeeds. Race condition?
+int tries = 0;
+IJSObjectReference? module = null;
+while (module is null && tries < 5)
+{
+    Console.WriteLine($"Trying to import JS module");
+    module = await js.InvokeAsync<IJSObjectReference>("import", "./localeHelper.js");
+    tries++;
+}
+
+string message = "";
+if (module is not null)
+    message = await module.InvokeAsync<string>("showPrompt", "hello");
+
+Console.WriteLine($"Message: {message}");
+
+await host.RunAsync();
---
+++ Client/wwwroot/localeHelper.js
@@ -0,0 +1,4 @@
+export function showPrompt(message) {
+  alert(message);
+  return message;
+}
